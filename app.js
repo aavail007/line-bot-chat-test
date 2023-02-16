8bf62b49a0a462470798fd10b8d7ea4f96d714ae
陳俐孟 <Limeng.Chen@auocare.com>
2023-02-16 16:26:44 +0800

    調整 json 格式

--- app.js
+++ app.js
@@ -67,17 +67,26 @@ async function handleEvent(event) {
   }
 
   if (regex.test(event.message.text)) { // 有 []包起來表示不須經由 GPT 回覆
-    if(event.message.text === '[機構活動]') {
+    if(event.message.text === '[活動資料]') {
       textString = demoData.activityData;
-      replayObj = buildFlexMsgObj('機構活動', textString);
-    } else if (event.message.text === '[生命徵象]') {
+      replayObj = buildFlexMsgObj('活動資料', textString);
+    } else if (event.message.text === '[健康資料]') {
       textString = demoData.vitalsignData;
-      replayObj = buildFlexMsgObj('生命徵象', textString);
-    } else if (event.message.text === '[關於我們]') {
+      replayObj = buildFlexMsgObj('健康資料', textString);
+    } else if (event.message.text === '[機構資訊]') {
       textString = demoData.aboutUs;
       replayObj = { type: 'text', text: textString };
-    } else if('[userid]') {
+    } else if(event.message.text === '[基本資料]') {
+      textString = demoData.memberInfo;
+      replayObj = buildFlexMsgObj('基本資料', textString);
+    } else if(event.message.text === '[操作說明]') {
+      textString = demoData.instructions;
+      replayObj = buildFlexMsgObj('操作說明', textString);
+    } else if(event.message.text === '[userid]') {
       replayObj = { type: 'text', text: '您的 userId = ' + userId };
+    } else if(event.message.text === '[切換住民]') {
+      textString = demoData.changeResident;
+      replayObj = buildFlexMsgObj('切換住民', textString);
     } else {
       replayObj = { type: 'text', text: '很抱歉，沒有對應這個指令的回覆' };
     }
@@ -108,9 +117,12 @@ async function getDemoData() {
   var api_url = `https://sheets.googleapis.com/v4/spreadsheets/${googleEnv.googleSheetId}/values/${googleEnv.googleSheetName}?alt=json&key=${googleEnv.googleKey}`;
   let data = await axios.get(api_url);
   demoData = {
-    activityData: data.data.values[0][1],
-    vitalsignData: data.data.values[1][1],
-    aboutUs: data.data.values[2][1],
+    activityData: JSON.parse(JSON.stringify(data.data.values[0][1])),
+    vitalsignData: JSON.parse(JSON.stringify(data.data.values[1][1])),
+    aboutUs: JSON.parse(JSON.stringify(data.data.values[2][1])),
+    memberInfo: JSON.parse(JSON.stringify(data.data.values[3][1])),
+    instructions: JSON.parse(JSON.stringify(data.data.values[4][1])),
+    changeResident: JSON.parse(JSON.stringify(data.data.values[5][1])),
   }
   return data;
 }
