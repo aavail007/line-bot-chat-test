93ad819617dbb9ce1e18fd6710307647a6b5fd67
陳俐孟 <Limeng.Chen@auocare.com>
2023-02-18 17:00:57 +0800

    調整綁定長者文案

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ async function msgEvent(event) {
     if(event.message.text.includes('false')) {
       replayObj = { type: 'text', text: '驗證碼錯誤' };
     } else {
-      replayObj = { type: 'text', text: '綁定成功' };
+      replayObj = { type: 'text', text: '綁定成功，請點選圖文選單功能來取得長者資訊' };
     }
   } else { // openai GPT 回
     const completion = await openai.createCompletion({
@@ -127,7 +127,7 @@ function postbackEvent(event) {
   console.log("++++++++ postbackEvent event data +++++++++", data);
   // 根据 postback 資料执行相应的操作
   if (data === 'bindMember') {
-    replayObj = { type: 'text', text: '請輸入要綁定的長者身分證字號(請勿刪除輸入框的預設文字)' };
+    replayObj = { type: 'text', text: '請輸入要綁定的長者身分證字號與驗證碼\n※手機版請勿刪除輸入框的預設文字 \n ※桌機版請依照下列格式回覆\n 請輸入身分證:\n O123456789\n請輸入驗證碼: 123456\n' };
     console.log("++++++++ postbackEvent replayObj +++++++++", replayObj);
     return client.replyMessage(event.replyToken, replayObj);
   } else if (data === 'ACTION_2') {
